f897d3068ee4d8cd723add46ba2ac3d1bc07fcd1
svaillo2 <svaillo2@edu.univ-fcomte.fr>
2025-04-15 14:28:22 +0200

    suppression de l'api en python pour en faire une nouvelle en js

--- elekable_api/app/repositories/user_repository.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import Session, select
-from app.models.user_model import User
-
-def get_user_by_email(session: Session, email: str):
-    return session.exec(select(User).where(User.email == email)).first()
